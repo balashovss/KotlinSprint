4577dfb9c20cb0ad4596edd2cf87e43ffc3ac0e1
Semakiller <semen.balashov.02@gmail.com>
2025-05-04 21:43:59 +0300

    KS-1-5 Fix: Made better logic in hours/minutes/seconds counting

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -2,9 +2,11 @@ package lesson1
 
 fun main() {
     val seconds: Short = 6480
-    val hoursTime: Short = (6480 / 3600).toShort()
-    val minutesTime: Short = ((seconds - hoursTime * 3600) / 60).toShort()
-    val secondsTime: Short = (seconds - hoursTime * 3600 - minutesTime * 60).toShort()
+    val secondsInMinute = 60
+    val minutesInHour = 60
+    val hoursTime: Short = (seconds / (secondsInMinute * minutesInHour)).toShort()
+    val minutesTime: Short = ((seconds / secondsInMinute) % minutesInHour).toShort()
+    val secondsTime: Short = (seconds % secondsInMinute).toShort()
 
-    println(String.format("%02d:%02d:%02d", hoursTime, minutesTime, secondsTime))
+    println("%02d:%02d:%02d".format(hoursTime, minutesTime, secondsTime))
 }
