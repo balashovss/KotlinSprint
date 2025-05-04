7a085ea23d8ab8204588ffaf044da1d0aaf1102f
Semakiller <semen.balashov.02@gmail.com>
2025-05-04 23:34:20 +0300

    KS-2-4 Feature: implemented task4

---
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -0,0 +1,10 @@
+package lesson2
+
+fun main() {
+    val numberOfCrystals = 7
+    val numberOfIron = 11
+    val buffPercent = 20
+
+    println("Bonus crystals:" + (numberOfCrystals * buffPercent) / 100)
+    println("Bonus irons:" + (numberOfIron * buffPercent) / 100)
+}
