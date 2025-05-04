d5a26bdfd88be4e97d913ba064089c9248b05646
Semakiller <semen.balashov.02@gmail.com>
2025-05-04 18:15:37 +0300

    KS-1-2 Fix: Reformatted code

--- src/main/kotlin/lesson1/lesson1_task2.kt
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -1,11 +1,13 @@
 package lesson1
 
 fun main() {
-    var numberOfOrders : Int = 75 //по логике должна изменяться
+    val numberOfOrders: Int = 75
     val thankfulText: String = "Thanks for your order!"
+
     println("Number of orders: $numberOfOrders")
     println("Order text: $thankfulText")
     var numberOfWorkers: Int = 2000
+
 //    println("Number of workers: $numberOfWorkers")
     numberOfWorkers--
     println("Number of workers: $numberOfWorkers")
