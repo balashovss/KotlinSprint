7da3ac199dfadd7c9204f3bab126f651fb1d9276
Semakiller <semen.balashov.02@gmail.com>
2025-05-04 23:08:13 +0300

    KS-2-2 Feature: implemented second task

---
+++ src/main/kotlin/lesson2/lesson_2_task2.kt
@@ -0,0 +1,15 @@
+package lesson2
+
+fun main() {
+    val numberOfWorkers = 50
+    val workerFee = 30000
+    val numberOfInterns = 30
+    val internFee = 20000
+    val workersSummaryFee = numberOfWorkers * workerFee
+    val internsSummaryFee = numberOfInterns * internFee
+    val summaryFee = workersSummaryFee + internsSummaryFee
+
+    println(workersSummaryFee)
+    println(summaryFee)
+    println(summaryFee / (numberOfWorkers + numberOfInterns))
+}
