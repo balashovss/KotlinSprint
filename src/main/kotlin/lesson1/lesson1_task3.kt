9bc2c07fdcf951911f3cbc64ce55ec7fcdf123bb
Semakiller <semen.balashov.02@gmail.com>
2025-05-04 18:25:26 +0300

    KS-1-3 Fix: Reformatted code + changed type of year, hour and minute

--- src/main/kotlin/lesson1/lesson1_task3.kt
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -1,13 +1,16 @@
 package lesson1
 
-
 fun main() {
-    val year = 1961
-    var hour = 9
-    var minute = 7
+    val year = "1961"
+    var hour = "09"
+    var minute = "07"
+
     println(year)
-    println(String.format("%02d\n%02d", hour, minute))
-    hour = 10
-    minute = 55
-    print(String.format("%02d:%02d", hour, minute))
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+
+    print("$hour:$minute")
 }
