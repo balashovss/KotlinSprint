0b51261262795666965de646b1f7864ade8da660
Semakiller <semen.balashov.02@gmail.com>
2025-05-04 18:06:08 +0300

    KS-1-1 Fix: Reformatted code + changed type of numberOfOrders

--- src/main/kotlin/lesson1/lesson1_task1.kt
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -1,6 +1,6 @@
 package lesson1
 
 fun main() {
-    var numberOfOrders : Int = 75 //по логике должна изменяться
+    val numberOfOrders: Int = 75
     val thankfulText: String = "Thanks for your order!"
 }
