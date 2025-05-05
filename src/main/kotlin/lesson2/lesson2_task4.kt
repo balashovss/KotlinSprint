c0f0b00bfe641124e67fbcc8699af176d4fbb0c8
Semakiller <semen.balashov.02@gmail.com>
2025-05-05 19:30:29 +0300

    KS-2-4 Fix: made a new const

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -1,10 +1,11 @@
 package lesson2
 
+const val PERCENTAGE = 100
 fun main() {
     val numberOfCrystals = 7
     val numberOfIron = 11
     val buffPercent = 20
 
-    println("Bonus crystals:" + (numberOfCrystals * buffPercent) / 100)
-    println("Bonus irons:" + (numberOfIron * buffPercent) / 100)
+    println("Bonus crystals:" + (numberOfCrystals * buffPercent) / PERCENTAGE)
+    println("Bonus irons:" + (numberOfIron * buffPercent) / PERCENTAGE)
 }
