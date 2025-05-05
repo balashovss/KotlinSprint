6e65ab37ada4aa271cf51d36abadb2dc83519431
Semakiller <semen.balashov.02@gmail.com>
2025-05-05 19:50:43 +0300

    KS-3-1 Fix: added template formatted strings

--- src/main/kotlin/lesson3/lesson3_task1.kt
+++ src/main/kotlin/lesson3/lesson3_task1.kt
@@ -1,16 +1,14 @@
 package lesson3
 
-const val COMA = ", "
-
 fun main() {
     val userName = "BalashovSS"
     val morningGreeting = "Good day"
     val dayGreeting = "Good evening"
-    var greeting = morningGreeting + COMA + userName
+    var greeting = "$morningGreeting, $userName"
 
     println(greeting)
 
-    greeting = dayGreeting + COMA + userName
+    greeting = "$dayGreeting, $userName"
 
     println(greeting)
 }
