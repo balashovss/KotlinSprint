9b0b22deb30bca99bd5d6740dc8f7208b0a04ba7
Semakiller <semen.balashov.02@gmail.com>
2025-05-06 22:44:15 +0300

    KS-4-3 Feature: implemented fourth task

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -1,12 +1,15 @@
 package lesson4
 
+const val IS_SUNNY = true
+const val IS_OPENED = true
 const val GOOD_HUMIDITY = 20
 const val BAD_SEASON = "зима"
+
 fun main() {
-    val isSunny = true
-    val isOpened = true
-    val humidity = 20
-    val season = "зима"
+    val isSunny = IS_SUNNY
+    val isOpened = IS_OPENED
+    val humidity = GOOD_HUMIDITY
+    val season = BAD_SEASON
     val isConditionGood = isSunny &&
             isOpened &&
             humidity == GOOD_HUMIDITY &&
